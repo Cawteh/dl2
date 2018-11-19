14256f5b6b8e17732a5de81f3722ddf797ee2764
Cawteh <1915biglight@gmail.com>
2018-11-19 23:38:18 +0900

    値の初期化部分を追加

--- 1-2.py
+++ 1-2.py
@@ -2,3 +2,8 @@ import numpy as np
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
+x = np.ramdom.randn(10, 2)
+W1 = np.random.randn(2, 4)
+b1 = np.random.randn(4)
+W2 = np.random.randn(4, 3)
+b2 = np.random.randn(3)
